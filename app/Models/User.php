57a7188e0ac575bc4191b888c66dd6db611d27b7
lujiancai <770166067@qq.com>
2019-09-06 07:34:13 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,38 @@ class User extends Authenticatable
 	public function feed(){
 		return $this->statuses ()->orderBy ('created_at','desc');
 	}
+
+	//获取粉丝
+	public function followers(){
+		return $this->belongsToMany (User::class,'followers','user_id','follower_id');
+	}
+
+	//获取关注的人
+	public function followings()
+	{
+		return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+	}
+
+	//关注
+	public function follow($user_ids)
+	{
+		if ( ! is_array($user_ids)) {
+			$user_ids = compact('user_ids');
+		}
+		$this->followings()->sync($user_ids, false);
+	}
+
+	//取消关注
+	public function unfollow($user_ids)
+	{
+		if ( ! is_array($user_ids)) {
+			$user_ids = compact('user_ids');
+		}
+		$this->followings()->detach($user_ids);
+	}
+
+	public function isFollowing($user_id)
+	{
+		return $this->followings->contains($user_id);
+	}
 }
